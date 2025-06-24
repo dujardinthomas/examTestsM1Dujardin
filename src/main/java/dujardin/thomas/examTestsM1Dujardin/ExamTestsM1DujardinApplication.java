9544ddc8a317939b83c8937b0256c6de60903640
Thomas Dujardin <thomasdujardin2003@gmail.com>
2025-06-24 09:55:46 +0200

    add spring-boot and docker-compose mysql

---
+++ src/main/java/dujardin/thomas/examTestsM1Dujardin/ExamTestsM1DujardinApplication.java
@@ -0,0 +1,13 @@
+package dujardin.thomas.examTestsM1Dujardin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamTestsM1DujardinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamTestsM1DujardinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dujardin/thomas/examTestsM1Dujardin/ExamTestsM1DujardinApplicationTests.java
@@ -0,0 +1,13 @@
+package dujardin.thomas.examTestsM1Dujardin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamTestsM1DujardinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
